3a1927f7d08cf920eac2008c76e8590d81a3a875
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-05-16 20:29:30 -0700

    finished functioning calculator

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(document).ready(initializeCalc);
 
 var input = [];
+var lastNumber = null;
+var lastOperator = null;
 
 function initializeCalc() {
     addEventhandlers();
@@ -16,11 +18,23 @@ function addEventhandlers() {
 function handleNumbers() {
     var text = $(this).text();
     var lastInputIndex = input.length-1;
-
+    if(typeof input[0] === 'number'){
+        input = [];
+        lastOperator = null;
+        lastNumber = null;
+    }
+    if (input[lastInputIndex] % 1 !== 0 && isNaN(text)){
+        //checks to see if float is true. if it has a remainder then yes.
+        //get out and don't add to it
+        return;
+    }
     if(isNaN(input[lastInputIndex])){
         input.push(text);
-    }else if (input[lastInputIndex] > 0){
-        input[lastInputIndex] += text;
+    }else if (input[lastInputIndex].length > 0){
+        if(input[input.length-1][input[input.length-1].length-1] !== text){
+            input[lastInputIndex] += text;
+        }
+
     }
     console.log('value: ', input);
     updateDisplay();
@@ -28,9 +42,16 @@ function handleNumbers() {
 
 function handleOperator(){
     var text = $(this).text();
-    console.log('value: ', text)
+    if(input.length === 0){
+        return
+    }else if(isNaN(input[input.length-1])){
+        input.pop();
+    }
     input.push(text);
+    lastOperator = text;
+    lastNumber = input[0];
     updateDisplay();
+    console.log('!!!handleOperator: ',lastNumber);
 }
 
 function handleClearButtons() {
@@ -46,17 +67,37 @@ function handleClearButtons() {
 
 function handleEqual() {
     var output = input[0];
-    for(var i = 1; i<input.length-1; i+=2){
-        output = operatorLogic(output,input[i],input[i+1]);
+
+    if (input.length === 0) {
+        output = 'ready';
+    }else if(input.length <= 2){//needs to check for [1,+]
+        console.log("i'm just hitting =");
+        if(lastNumber && lastOperator){
+        input[0] = operatorLogic(input,lastOperator,lastNumber);
+        console.log('just hitting = ',input[0]);
+        }
+    }else if(input.length > 1) {
+        for (var i = 1; i < input.length - 1; i += 2) {
+            debugger;
+            output = operatorLogic(output, input[i], input[i + 1]);
+            if(isNaN(input[input.length-1])){
+                lastNumber = operatorLogic(output, lastOperator, output);
+                output = lastNumber;
+            }else{
+                lastNumber = input[i+1];
+            }
+            console.log('1+1+ ',lastNumber);
+        }
+        input = [output];
     }
-    console.log(output);
-    return output;
+    updateDisplay(output);
 }
 
 function operatorLogic(_inputNum1, _operator, _inputNum2) {
     var output;
     var num1 = parseFloat(_inputNum1);
     var num2 = parseFloat(_inputNum2);
+    console.log('num1: ',num1, 'num2: ', num2 );
     switch (_operator){
         case '+':
             output = num1 + num2;
@@ -64,18 +105,20 @@ function operatorLogic(_inputNum1, _operator, _inputNum2) {
         case '-':
             output = num1 - num2;
             break;
-        case 'X':
+        case 'x':
             output = num1 * num2;
             break;
         case '÷':
             output = num1 / num2;
+            if(output === Infinity){
+                output = 'error'
+            }
             break;
     }
     return output;
 }
 
-function updateDisplay() {
-    var inputText = input.join();
-    var outputText =inputText.replace(/,/g,'');
-    $('#input').text(outputText);
+function updateDisplay(_textToUpdate) {
+    var inputText = _textToUpdate ? _textToUpdate : input[input.length-1];
+    $('#input').text(inputText);
 }
