5fb51f12391ea18a9813ba6af118e90aeaf818f3
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-05-18 12:58:52 -0700

    added Pemdas and fixed 0. bug

--- main.js
+++ main.js
@@ -24,6 +24,9 @@ function resetCalculatorVariables() {
 }
 
 function isFloat(_inputNumStr){// checks to see if number is a float.
+    if(_inputNumStr === undefined){
+        return false;
+    }
     _inputNumStr += 1; // adds 1 to string to make it 1.1
     return _inputNumStr % 1 !== 0;//checks to see if 1.1 is a float.
 }
@@ -33,12 +36,19 @@ function handleNumbers() {
     var lastInputIndex = input.length-1;
 
     if(typeof input[input.length-1] === 'number') {
+        //this checks to see if you already done an equal and resets for a new number.
         resetCalculatorVariables();
     }
 
     if (isFloat(input[lastInputIndex])&& isNaN(text)){
+        //return if float and '.' is pressed.
         return;
     }else if(isNaN(input[lastInputIndex])){
+        //checks for operator.
+        if(text === '.'){
+            //safety net for '.'
+            text = '0.'
+        }
         input.push(text);
     }else if (input[lastInputIndex].length > 0){
             input[lastInputIndex] += text;
@@ -72,7 +82,7 @@ function handleClearButtons() {
 function handleEqual() {
     var output = input[0];
 
-    if (input.length === 0) {
+    if (input.length === 0 || input[0] === NaN) {
         output = 'Ready';
     }else if(input.length <= 2){//needs to check for [1,+]
         if(lastNumber && lastOperator){
@@ -80,17 +90,9 @@ function handleEqual() {
         output = input[0]; // for display to update.
         }
     }else if(input.length > 1) {//if we have a long string of nums and operators.
-        // for (var i = 1; i < input.length - 1; i += 2) {
-        //     output = operatorLogic(output, input[i], input[i + 1]);
-        //     if(isNaN(input[input.length-1])){
-        //         lastNumber = operatorLogic(output, lastOperator, output);
-        //         output = lastNumber;
-        //     }else{
-        //         lastNumber = input[i+1];
-        //     }
-        // }
-
-        output = linearDoMath(output);
+        //debugger;
+        //output = linearDoMath(output);
+        output = pemdasDoMath(output);
         input = [output];
     }
     updateDisplay(output);
@@ -100,7 +102,7 @@ function linearDoMath(_output){
     for (var i = 1; i < input.length - 1; i += 2) {
         _output = operatorLogic(_output, input[i], input[i + 1]);
         if(isNaN(input[input.length-1])){
-            lastNumber = operatorLogic(_output, lastOperator, output);
+            lastNumber = operatorLogic(_output, lastOperator, _output);
             _output = lastNumber;
         }else{
             lastNumber = input[i+1];
@@ -108,9 +110,22 @@ function linearDoMath(_output){
     }
     return _output
 }
-function pemdasDoMath(){
-    console.log('yay')
+
+function pemdasDoMath(_output){
+    var pemdasMath;
+    var i = 1;
+    while(i < input.length){
+        if( input[i] === 'x' || input[i] === '÷'){
+            pemdasMath = operatorLogic( input[i-1],input[i],input[i+1] )
+            input.splice(i-1, 3,pemdasMath);
+            i = 1;
+        }
+        i+=2;
+    }
+    _output = linearDoMath(input[0]);
+    return _output;
 }
+
 function operatorLogic(_inputNum1, _operator, _inputNum2) {
     var output;
     var num1 = parseFloat(_inputNum1);
