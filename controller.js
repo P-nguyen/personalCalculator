5efa6087123fb3d1246eebacc867cf60d6e26a19
Pangu <panguyenuwm@gmail.com>
2018-07-29 13:02:13 -0700

    fixed MVC format

--- controller.js
+++ controller.js
@@ -1,8 +1,52 @@
 class Controller {
     constructor() {
         this.calculator = new Calculator();
-        this.view = new View(this);
+        this.view = new View();
+
+        this.handleNumbers = this.handleNumbers.bind(this);
+        this.handleOperator = this.handleOperator.bind(this);
+        this.handleClearButtons = this.handleClearButtons.bind(this);
+        this.handleEqual = this.handleEqual.bind(this);
+        this.toggleCalcState = this.toggleCalcState.bind(this);
+        this.initializeCalc();
     }
+
+    initializeCalc() {
+        $(document).ready(this.addEventhandlers.bind(this));
+    }
+
+    addEventhandlers() {
+        $('.numbers .button').on('click', this.handleNumbers);
+        $('.operators .button').on('click', this.handleOperator);
+        $('.clearEntry .button').on('click', this.handleClearButtons);
+        $('.equal .button').on('click', this.handleEqual);
+        $('#togglePemdas').on('click', this.toggleCalcState);
+    }
+
+    toggleCalcState() {
+        this.handleToggle();
+    }
+
+    handleNumbers(event) {
+        var text = event.currentTarget.innerText;
+        this.handleInput(text);
+    }
+
+    handleOperator(event) {
+        var text = event.currentTarget.innerText;
+        this.handleInput(text);
+    }
+
+    handleClearButtons() {
+        var text = event.currentTarget.innerText;
+        this.handleInput(text);
+    }
+
+    handleEqual() {
+        var text = event.currentTarget.innerText;
+        this.handleInput(text);
+    }
+
 
     handleInput(_textInput){
         var result;
@@ -27,4 +71,3 @@ class Controller {
     }
 
 }
-var controller = new Controller();
